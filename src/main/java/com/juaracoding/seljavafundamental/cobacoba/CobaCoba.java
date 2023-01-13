580c39bc63b46cfc87a3a42ac4268693ab4975b3
safrilefendilubis <safrilefendi20@gmail.com>
2023-01-13 18:49:04 +0700

    buat di cek

---
+++ src/main/java/com/juaracoding/seljavafundamental/cobacoba/CobaCoba.java
@@ -0,0 +1,170 @@
+package com.juaracoding.seljavafundamental.cobacoba;
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author User a.k.a. Safril Efendi Lubis
+Java Developer
+Created on 1/12/2023 7:25 PM
+@Last Modified 1/12/2023 7:25 PM
+Version 1.1
+*/
+public class CobaCoba {
+
+    public static void main(String[] args) {
+//        int x = 20;
+//        if (x % 2 == 0)
+//            if (x / 100 != 0)
+//                System.out.println(x + "BILANGAN GENAP LEBIH DARI SERATUS");
+//        else
+//            if (x / 10 != 0)
+//                System.out.println(x + "BILANGAN GENAP LEBIH DARI SEPULUH");
+//   int a = 10;
+//   int b = 5;
+//   boolean hasil;
+
+//   hasil = a == b;
+//        System.out.println("APAKAH a == b ?" + hasil );
+//
+//    hasil = a != b;
+//        System.out.println("Apakah a != b ? " + hasil);
+
+//       boolean isAnd = ((1!= 7)== false)&&(!(3>5)!=false);
+//        System.out.println(isAnd);
+
+//        boolean isOr = (5==5) || (4==3);
+//        System.out.println(isOr);
+
+//        int intPanjang = 0;
+//        int intLebar = 0;
+//        int intLuasPersegiPanjang = 0;
+//        System.out.println("NILAI PANJANG SEBELUM DIINPUT ADALAH"+intPanjang);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("MASUKKAN NILAI PANJANG : ");
+//        intPanjang = sc.nextInt();
+////        System.out.println("NILAI PANJANG ADALAH : "+intPanjang);
+//        System.out.print("MASUKKAN NILAI LEBAR : ");
+//        intLebar = sc.nextInt();
+//        System.out.println("LUAS PERSEGI PANJANG ADALAH : "+(intPanjang*intLebar));
+
+
+//        int intPanjangBesi = 0;
+//        int intBesiDibutuhkan = 0;
+//        System.out.println("PANJANG BESI SEBELUM DI GUNAKAN"+intPanjangBesi);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("MASUKKAN PANJANG BESI : ");
+//        intPanjangBesi = sc.nextInt();
+//        System.out.print("PANJANG BESI YANG DIBUTUHKAN : ");
+//        intBesiDibutuhkan = sc.nextInt();
+//        System.out.println("PANJANG BESI SESUDAH DIGUNAKAN"+(intPanjangBesi-intBesiDibutuhkan));
+
+//
+//            for(int j=0;j<5;j++)
+//            {
+//                if(j==3)
+//                {
+//                    System.out.println(" Ini yang terlompati "+j);
+//                    continue;
+//                }
+//                System.out.println(" 1 kali loop ke "+j);
+
+//        Scanner sc = new Scanner(System.in);
+//        int intNumber = 0;
+//        int intTotal = 0;
+//        boolean isAgain = true;
+//
+//        try {
+//            System.out.println("Masukkan angka sepuasnya: ");
+//            do {
+//                intNumber = sc.nextInt();
+//
+//                intTotal += intNumber;
+//            }while (isAgain);
+//        }catch (Exception e) {
+//            System.out.println("Jumlah: " + intTotal);
+//            System.exit(0);
+//        }
+
+//        int time = 20;
+//
+//        String result = (time < 18) ? "Selamat siang." : "Selamat malam.";
+//
+//        System.out.println(result);
+
+
+//        String nama = "Satibi";
+//
+//        if (nama.length()>3) {
+//
+//            System.out.println(nama.substring(4));
+//
+//        }
+
+//        int korupsi = 138000000;
+//
+//        if (korupsi < 1000000) {
+//
+//            System.out.println("Penjara 1 tahun");
+//
+//        }else {
+//
+//            if (korupsi< 100000000) {
+//
+//                System.out.println("Penjara 5 tahun");
+//
+//            }else {
+//
+//                if (korupsi < 1000000000) {
+//
+//                    System.out.println ("Penjara 10 tahun");
+//
+//                }else {
+//
+//                    System.out.println("Penjara 20 tahun");
+//
+//                }
+//
+//            }
+//
+//        }
+//
+//        if (korupsi > 150000000) {
+//
+//            System.out.println("Hukuman kebiri");
+//
+
+
+//        for (int i = 1; i < 5; i++) {
+//            System.out.print(i + ",");
+//        }
+
+//        int angka = 48;
+//
+//        if(angka%2==0)
+//        {
+//            System.out.println("Angka "+angka+" adalah bilangan genap");
+//        }
+//        else {
+//            System.out.println("Angka " + angka + " adalah bilangan ganjil");
+//        }
+
+//        int [] array = {2, 7, 3, 5, 8, 9};
+//
+//        int kuncen = array[0];
+//
+//        for (int i = 0; i < array.length; i++)
+//
+//        {
+//
+//            if (array[i] > kuncen)
+//
+//                kuncen = array[i];
+//
+//        }
+//
+//        System.out.print(kuncen);
+
+
+    }
+}
+
