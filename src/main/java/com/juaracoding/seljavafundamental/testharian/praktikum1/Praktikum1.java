85cc6798a83a2d157b2c20bc2df4f5dd9cd28b4d
safrilefendilubis <safrilefendi20@gmail.com>
2023-01-13 19:07:02 +0700

    buat di cek

---
+++ src/main/java/com/juaracoding/seljavafundamental/testharian/praktikum1/Praktikum1.java
@@ -0,0 +1,16 @@
+package com.juaracoding.seljavafundamental.testharian.praktikum1;
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author User a.k.a. Safril Efendi Lubis
+Java Developer
+Created on 1/13/2023 6:59 PM
+@Last Modified 1/13/2023 6:59 PM
+Version 1.1
+*/
+public class Praktikum1 {
+    public static void main(String[] args) {
+
+    }
+}
