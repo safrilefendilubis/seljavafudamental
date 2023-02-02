173887607a1fd8e9b1ca6e895f129a16b7e6f585
safrilefendilubis <safrilefendi20@gmail.com>
2023-02-02 17:13:04 +0700

    Praktikum4

--- src/main/java/com/juaracoding/seljavafundamental/testharian/praktikum_4/Praktikum4.java
+++ src/main/java/com/juaracoding/seljavafundamental/testharian/praktikum_4/Praktikum4.java
@@ -2,6 +2,7 @@ package com.juaracoding.seljavafundamental.testharian.praktikum_4;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 /*
 IntelliJ IDEA 2022.3.1 (Community Edition)
@@ -12,42 +13,100 @@ Created on 1/31/2023 2:36 PM
 @Last Modified 1/31/2023 2:36 PM
 Version 1.1
 */
-public class Praktikum4 {
-    public static void main(String[] args) {
-        String strKonsonan = "bcdfghjklmnpqrstvwxyz";
-        String strAlphabet ="abcdefghijklmnopqrstuvwxyz";
-        String strVokal = "aiueo";
-        String strTandaSambung = "_.";
-        String strAgka = "0123456789";
-        String[] strProvider = {"@gmail", "@hotmail", "@yahoo", "@ymail"};
-        String strDot = ".";
-        int intKonsonan = strKonsonan.length()-1;
-        int intAlphabet = strAlphabet.length()-1;
-        int intVokal = strVokal.length()-1;
-        int intTandaSambung = strTandaSambung.length()-1;
-        int intAngka = strAgka.length()-1;
-        int intDot = strDot.length()-1;
-        int intProvider = strProvider.length-1;
-        String strOutput ="";
-        Random rand = new Random();
-        int intPenampung = rand.nextInt(2);
-        int intTotalLoop = rand.nextInt(25,40);
 
-        for (int i=0;i<intTotalLoop;i++){
-            if(i<7){
-                if(intPenampung==1){
-                    intPenampung=2;
-                    strOutput += strKonsonan.charAt(rand.nextInt(intKonsonan));
-                }else {
-                    intPenampung = 1;
-                    strOutput += strVokal.charAt(rand.nextInt(intVokal));
-                }
-            }else{
-                strOutput += strTandaSambung.charAt(rand.nextInt(intTandaSambung));
-                strOutput += strAlphabet.charAt(rand.nextInt(intAlphabet));
-                strOutput += strDot.charAt(rand.nextInt(intDot));
-                strOutput += strProvider[rand.nextInt(intProvider)];
-            }
+public class Praktikum4 {
+    /*
+	Soal Nomor 1 Praktikum 4 Java Fundamental
+*/
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int intPilih;
+        int r;
+        int panjang;
+        int lebar;
+        int sisi;
+        int alas;
+        int tinggi;
+        int diagonalSatu;
+        int diagonalDua;
+
+        System.out.println("Pilih luas bangun datar : \n 1. Lingkaran \n 2. Persegi Panjang \n 3. Persegi \n 4. Segitiga \n 5. Belah Ketupat");
+        System.out.print("Masukan nomor bangun datar yang akan di hitung: ");
+        intPilih = sc.nextInt();
+        System.out.println("==========================================");
+
+        switch (intPilih) {
+            case 1:
+                System.out.println("Rumus luas lingkaran adalah : L = π * r²");
+                System.out.print("Masukan jari-jari lingkaran : ");
+                r = sc.nextInt();
+                System.out.println("Luas lingkaran : " + getLingkaran(r) + " cm²");
+                break;
+            case 2:
+                System.out.println("Rumus luas persegi panjang adalah : L = P * L");
+                System.out.print("Masukan panjang : ");
+                panjang = sc.nextInt();
+                System.out.print("Masukan lebar : ");
+                lebar = sc.nextInt();
+                System.out.println("Luas persegi panjang : " + getPersegiPanjang(panjang, lebar) + " cm²");
+                break;
+            case 3:
+                System.out.println("Rumus luas persegi adalah : L = S * S");
+                System.out.print("Masukan sisi : ");
+                sisi = sc.nextInt();
+                System.out.println(getPersegi(sisi) + "cm²");
+                break;
+            case 4:
+                System.out.println("Rumus luas segitiga adalah : L = ½ * A * T ");
+                System.out.print("Masukan alas : ");
+                alas = sc.nextInt();
+                System.out.print("Masukan tinggi : ");
+                tinggi = sc.nextInt();
+                System.out.println("Luas segitiga : " + getSegitiga(alas, tinggi) + " cm²");
+                break;
+            case 5:
+                System.out.println("Rumus luas belah ketupat adalah : L = ½ * d1 * d2");
+                System.out.print("Masukan diagonal 1 : ");
+                diagonalSatu = sc.nextInt();
+                System.out.print("Masukan diagonal 2 : ");
+                diagonalDua = sc.nextInt();
+                System.out.println("Luas belah ketupat : " + getBelahKetupat(diagonalSatu, diagonalDua) + " cm²");
+                break;
+            default:
+                System.out.println("Maaf angka tidak ada di pilihan!");
+
         }
     }
+
+    private static double getLingkaran(int r) {
+        double hasil;
+        hasil = 3.14 * Math.pow(r, 2);
+        return hasil;
+    }
+
+    private static double getPersegiPanjang(int panjang, int lebar) {
+        int hasil;
+        hasil = panjang * lebar;
+        return hasil;
+    }
+
+    private static double getPersegi(int sisi) {
+        double hasil;
+        hasil = Math.pow(sisi, 2);
+        return hasil;
+    }
+
+    private static double getSegitiga(int alas, int tinggi) {
+        double hasil;
+        hasil = 0.5 * alas * tinggi;
+        return hasil;
+    }
+
+    private static double getBelahKetupat(int d1, int d2) {
+        double hasil;
+        hasil = 0.5 * d1 * d2;
+        return hasil;
+    }
 }
+
